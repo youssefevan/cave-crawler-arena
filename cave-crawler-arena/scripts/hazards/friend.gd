39126e63eba974ebf4048cfc2981663c069a6b9c
Evan <evanyoussef8@gmail.com>
2026-01-21 00:26:43 -0500

    feat: fish boss base art
    - added fish boss base art
    - added friend item art
    - tweaked misc sprites

---
+++ cave-crawler-arena/scripts/hazards/friend.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+var player : Player
+var speed := 150.0
+
+func _physics_process(delta):
+	var circling = (global_position - player.global_position).normalized().rotated(PI/2) * speed
+	
+	var encroach_speed = 8.0
+	if global_position.distance_to(player.global_position) > 130.0:
+		encroach_speed = 30.0
+	else:
+		encroach_speed = 8.0
+	
+	var move_in = global_position.direction_to(player.global_position) * 8
+	velocity = circling + move_in
+	
+	move_and_slide()
